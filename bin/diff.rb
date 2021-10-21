d392c1c6228d0354d0a34ec29a807ac223cdbc06
Tony Bowden <tony@tmtm.com>
2021-10-21 10:14:40 +0300

    Add parliamentary groups

--- bin/diff.rb
+++ bin/diff.rb
@@ -3,5 +3,18 @@
 
 require 'every_politician_scraper/comparison'
 
-diff = EveryPoliticianScraper::Comparison.new('data/wikidata.csv', 'data/official.csv').diff
+# standardise parties
+class Comparison < EveryPoliticianScraper::Comparison
+  REMAP = {
+    'Șor Party' => 'Fracțiunea parlamentară a Partidului Politic "ȘOR"',
+    'Electoral Bloc of Communists and Socialists' => 'Fracțiunea parlamentară a Blocului Comuniștilor și Socialiștilor',
+    'Party of Action and Solidarity' => 'Fracțiunea parlamentară "Partidul Acțiune și Solidaritate"'
+  }.freeze
+
+  def wikidata_csv_options
+    { converters: [->(val) { REMAP.fetch(val, val) }] }
+  end
+end
+
+diff = Comparison.new('data/wikidata.csv', 'data/official.csv').diff
 puts diff.sort_by { |r| [r.first, r.last.to_s] }.reverse.map(&:to_csv)
--- bin/scraper/wikidata.rb
+++ bin/scraper/wikidata.rb
@@ -4,12 +4,16 @@
 require 'every_politician_scraper/wikidata_query'
 
 query = <<SPARQL
-  SELECT (STRAFTER(STR(?member), STR(wd:)) AS ?item) ?name
+  SELECT (STRAFTER(STR(?member), STR(wd:)) AS ?item) ?name ?faction
     (STRAFTER(STR(?ps), '/statement/') AS ?psid)
   WHERE {
     ?member p:P39 ?ps .
     ?ps ps:P39 wd:Q18390049 ; pq:P2937 wd:Q107651615 .
     FILTER NOT EXISTS { ?ps pq:P582 ?end }
+    OPTIONAL {
+      ?ps pq:P4100 ?group .
+      OPTIONAL { ?group rdfs:label ?faction FILTER(LANG(?faction) = "en") }
+    }
 
     OPTIONAL { ?ps prov:wasDerivedFrom/pr:P1810 ?sourceName }
     OPTIONAL { ?member rdfs:label ?enLabel FILTER(LANG(?enLabel) = "en") }
